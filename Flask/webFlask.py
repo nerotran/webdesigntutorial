06fa19479ebea0f559169b8d9e812a62c89ba9d7
profnero <59196646+profnero@users.noreply.github.com>
2019-12-25 04:36:14 -0500

    12/25/2019 04:35AM
    Flask: templates added, some other css and html stuff

--- Flask/webFlask.py
+++ Flask/webFlask.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        "title": "First post",
+        "author": "Nero",
+        "date": "Aug 10",
+        "content": "content"
+    },
+    {
+        "title": "Second post",
+        "author": "Nero",
+        "date": "Oct 10",
+        "content": "content 2"
+    }
+
+]
 
 
 @app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="about")
+
+
+#if run directly using python, launch in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
